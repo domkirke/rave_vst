e9ae397a9df6dfa9da7c4f0220f45842dc4fadfb
Axel Chemla--Romeu-Santos <axelchemla@yahoo.fr>
2023-12-26 00:13:10 +0100

    added compilation script

---
+++ compile.sh
@@ -0,0 +1,45 @@
+CONFIG=Release
+clean=0
+build=1
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --config=*)
+      CONFIG="${1#*=}"
+      ;;
+    --clean=*)
+      clean="${1#*=}"
+      ;;
+    --build=*)
+      build="${1#*=}"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
+
+
+if [ $clean -eq 1 ]; then
+    # Clean #
+    rm -rf build
+    rm -rf Libs/JUCE-*
+fi
+
+if [ $build -eq 1 ]; then
+    # Make build dir and copy libraries #
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_BUILD_TYPE=${CONFIG} -DCMAKE_OSX_ARCHITECTURES=x86_64\;arm64
+    cmake --build . --config ${CONFIG} -j 4
+fi
+
+if [[ "$OSTYPE" =~ ^darwin* ]]
+then
+  codesign -f --timestamp -s - build/rave-vst_artefacts/${CONFIG}/AU/RAVE.component/Contents/MacOS/RAVE
+  codesign -f --timestamp -s - build/rave-vst_artefacts/${CONFIG}/VST3/RAVE.vst3/Contents/MacOS/RAVE
+  codesign -f --timestamp -s - build/rave-vst_artefacts/${CONFIG}/Standalone/RAVE.app/Contents/MacOS/RAVE
+fi
