5e8927d791cfd6bed9d20b75d7ac60de1478e610
Axel Chemla--Romeu-Santos <axelchemla@yahoo.fr>
2023-12-26 00:02:19 +0100

    updated code for 10.15

--- cmake/add_torch.cmake
+++ cmake/add_torch.cmake
@@ -7,6 +7,18 @@ find_library(torch_lib
   PATHS ${torch_dir}/libtorch/lib
 )
 
+function (download_library url out)
+  message("download ${url} to ${out}...")
+  file(DOWNLOAD
+      ${url}
+      ${out}/torch_cc.zip
+      SHOW_PROGRESS
+    )
+    execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xf torch_cc.zip
+                    WORKING_DIRECTORY ${out})
+    file(REMOVE ${out}/torch_cc.zip)
+endfunction()
+
 if (DEFINED torch_version)
   message("setting torch version : ${torch_version}")
 else()
@@ -17,40 +29,36 @@ endif()
 if (NOT torch_lib)
   message(STATUS "Downloading torch C API pre-built")
 
-
   # Download
   if (UNIX AND NOT APPLE)  # Linux
     set(torch_url
         "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-${torch_version}%2Bcpu.zip")
+    download_library(${torch_url} ${torch_dir})
   elseif (UNIX AND APPLE)  # OSXx
-    set (APPLE_ARM64 (CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64"))
-    if (APPLE_ARM64)
-      set(torch_url
-          "https://anaconda.org/pytorch/pytorch/${torch_version}/download/osx-arm64/pytorch-${torch_version}-py3.10_0.tar.bz2")
-    else()
-      set(torch_url
-        "https://download.pytorch.org/libtorch/cpu/libtorch-macos-${torch_version}.zip")
+    # Check if architecutre == ARM64
+    if (NOT DEFINED APPLE_ARM64)
+      set (APPLE_ARM64 (CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64"))
     endif()
-    message("ARM64 ${CMAKE_SYSTEM_PROCESSOR} detected.\n torch download link : ${torch_url}")
-  else()                   # Windows
-    set(torch_url
-        "https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-${torch_version}%2Bcpu.zip")
+    ## If ARM, download both libraries and pre-compile UB libraries
+    if (APPLE_ARM64)
+      # download arm64 library
+      download_library("https://anaconda.org/pytorch/pytorch/${torch_version}/download/osx-arm64/pytorch-${torch_version}-py3.10_0.tar.bz2" ${torch_dir}-arm64)
+      execute_process(COMMAND mkdir ${torch_dir})
+      execute_process(COMMAND cp -r ${torch_dir}-arm64/lib/python3.10/site-packages/torch ${torch_dir}/libtorch)
+      # file(RENAME "${CMAKE_CURRENT_BINARY_DIR}/torch/lib/python3.10/site-packages/torch" "${CMAKE_CURRENT_BINARY_DIR}/torch/torch")
+      # file(RENAME "${CMAKE_CURRENT_BINARY_DIR}/torch/torch" "${CMAKE_CURRENT_BINARY_DIR}/torch/libtorch")
+      # download x86 library
+      download_library("https://download.pytorch.org/libtorch/cpu/libtorch-macos-${torch_version}.zip" ${torch_dir}-x86)
+      # export UB libs to main path
+      execute_process(COMMAND find ${torch_dir}/libtorch/lib -maxdepth 1 -type f -execdir lipo -create ${torch_dir}/libtorch/lib/{} ${torch_dir}-x86/libtorch/lib/{} -output ${torch_dir}/libtorch/lib/{} \;)
+      execute_process(COMMAND rm -rf ${torch_dir}-x86)
+      file(REMOVE ${torch_dir}-x86)
+    else()
+      download_library("https://download.pytorch.org/libtorch/cpu/libtorch-macos-${torch_version}.zip" ${torch_dir})
+    endif()
+  else()
+    download_library("https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-${torch_version}%2Bcpu.zip")
   endif()
-
-  file(DOWNLOAD
-    ${torch_url}
-    ${torch_dir}/torch_cc.zip
-    SHOW_PROGRESS
-  )
-  execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xf torch_cc.zip
-                  WORKING_DIRECTORY ${torch_dir})
-
-  file(REMOVE ${torch_dir}/torch_cc.zip)
-  if (APPLE_ARM64)
-    file(RENAME "${CMAKE_CURRENT_BINARY_DIR}/torch/lib/python3.10/site-packages/torch" "${CMAKE_CURRENT_BINARY_DIR}/torch/torch")
-    file(RENAME "${CMAKE_CURRENT_BINARY_DIR}/torch/torch" "${CMAKE_CURRENT_BINARY_DIR}/torch/libtorch")
-  endif()
-
 endif()
 
 # Find the libraries again
